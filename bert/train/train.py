c40509c858ba87051680f5395af0621ae34736ba
ztex <ztex030640417@gmail.com>
2019-07-29 12:53:35 +0800

    Fix state load error

--- bert/train/train.py
+++ bert/train/train.py
@@ -7,6 +7,7 @@ from .datasets.classification import SST2IndexedDataset
 from .trainer import Trainer
 from .utils.log import make_run_name, make_logger, make_checkpoint_dir
 from .utils.collate import pretraining_collate_function, classification_collate_function
+from .utils.stateload import stateLoading
 from .optimizers import NoamOptimizer
 
 import torch
@@ -17,7 +18,6 @@ from torch.utils.data import DataLoader
 import random
 import numpy as np
 from os.path import join
-
 
 RUN_NAME_FORMAT = (
     "BERT-"
@@ -182,6 +182,82 @@ def finetune(pretrained_checkpoint,
     trainer.run(epochs=epochs)
     return trainer
 
+def finetuneSPS(pretrained_checkpoint,
+             data_dir, train_path, val_path, dictionary_path,
+             vocabulary_size, batch_size, max_len, epochs, lr, clip_grads, device,
+             layers_count, hidden_size, heads_count, d_ff, dropout_prob,
+             log_output, checkpoint_dir, print_every, save_every, config, run_name=None, num_classes=2, **_):
+
+    random.seed(0)
+    np.random.seed(0)
+    torch.manual_seed(0)
+
+    train_path = train_path if data_dir is None else join(data_dir, train_path)
+    val_path = val_path if data_dir is None else join(data_dir, val_path)
+    dictionary_path = dictionary_path if data_dir is None else join(data_dir, dictionary_path)
+
+    run_name = run_name if run_name is not None else make_run_name(RUN_NAME_FORMAT, phase='finetune', config=config)
+    logger = make_logger(run_name, log_output)
+    logger.info('Run name : {run_name}'.format(run_name=run_name))
+    logger.info(config)
+
+    logger.info('Constructing dictionaries...')
+    dictionary = IndexDictionary.load(dictionary_path=dictionary_path,
+                                      vocabulary_size=vocabulary_size)
+    vocabulary_size = len(dictionary)
+    #logger.info(f'dictionary vocabulary : {vocabulary_size} tokens')
+    logger.info('dictionary vocabulary : {vocabulary_size} tokens'.format(vocabulary_size=vocabulary_size))
+
+    logger.info('Loading datasets...')
+    train_dataset = SST2IndexedDataset(data_path=train_path, dictionary=dictionary)
+    val_dataset = SST2IndexedDataset(data_path=val_path, dictionary=dictionary)
+    logger.info('Train dataset size : {dataset_size}'.format(dataset_size=len(train_dataset)))
+
+    logger.info('Building model...')
+    pretrained_model = build_model(layers_count, hidden_size, heads_count, d_ff, dropout_prob, max_len, vocabulary_size)
+    #pretrained_model.load_state_dict(torch.load(pretrained_checkpoint, map_location='cpu')['state_dict'])
+    pretrained_model = stateLoading(pretrained_model, pretrained_checkpoint)
+
+    model = FineTuneModel(pretrained_model, hidden_size, num_classes=num_classes)
+
+    logger.info(model)
+    logger.info('{parameters_count} parameters'.format(
+        parameters_count=sum([p.nelement() for p in model.parameters()])))
+
+    loss_model = ClassificationLossModel(model)
+    metric_functions = [classification_accuracy]
+
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        collate_fn=classification_collate_function)
+
+    val_dataloader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        collate_fn=classification_collate_function)
+
+    optimizer = Adam(model.parameters(), lr=lr)
+
+    checkpoint_dir = make_checkpoint_dir(checkpoint_dir, run_name, config)
+
+    logger.info('Start training...')
+    trainer = Trainer(
+        loss_model=loss_model,
+        train_dataloader=train_dataloader,
+        val_dataloader=val_dataloader,
+        metric_functions=metric_functions,
+        optimizer=optimizer,
+        clip_grads=clip_grads,
+        logger=logger,
+        checkpoint_dir=checkpoint_dir,
+        print_every=print_every,
+        save_every=save_every,
+        device=device
+    )
+
+    trainer.run(epochs=epochs)
+    return trainer
 
 def add_pretrain_parser(subparsers):
     pretrain_parser = subparsers.add_parser('pretrain')
