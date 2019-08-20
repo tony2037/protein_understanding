65ddd3e747c65d8cae69cc5b7da2b4bbd98ced19
ztex <ztex030640417@gmail.com>
2019-08-20 11:35:13 +0800

    Reconstruct Instead of functions package the operations make them nn.Module

--- bert/train/sigunet/utils.py
+++ bert/train/sigunet/utils.py
@@ -5,73 +5,122 @@ import numpy as np
 from sklearn.metrics import matthews_corrcoef, f1_score
 
 
-def conv1d(input, channels=None, kernel_size=None, act=nn.ReLU()):
-    """1d convoluation operation.
+class conv1d(nn.Module):
 
-    Arguments:
-        input (torch.Tensor): Input tensor to do convoluation.
-                                            Its shape need to be `(batch_size, channels, length)`.
-        channels (int): Number of channels.
-        kernel_size (int): Length of 1d kernel.
-        act (activation function): Activation function after convoluation. Default is `nn.ReLU`.
+    def __init__(self, channels=None, kernel_size=None, act=nn.ReLU()):
+        super(conv1d, self).__init__()
+        """1d convoluation operation.
 
-    Returns:
-        Tensor
-    """
+        Arguments:
+            input (torch.Tensor): Input tensor to do convoluation.
+                                                Its shape need to be `(batch_size, channels, length)`.
+            channels (int): Number of channels.
+            kernel_size (int): Length of 1d kernel.
+            act (activation function): Activation function after convoluation. Default is `nn.ReLU`.
 
-    assert (kernel_size & 1) == 1
-    in_channels = int(input.shape[1])
-    # Since there is no padding='SAME' such feature, refer to https://discuss.pytorch.org/t/convolution-1d-and-simple-function/11606/5
-    # which says that by setting padding=(kernel_size // 2) can nail same objective provided kernel size is odd
-    conv1d = nn.Conv1d(in_channels, channels, kernel_size=kernel_size, stride=1, padding=(kernel_size // 2)).cuda()(input)
-    activate = act(conv1d)
+        Returns:
+            Tensor
+        """
 
-    return activate
+        assert (kernel_size & 1) == 1
+        self.channels = channels
+        self.kernel_size = kernel_size
+        self.conv = None
+        self.act = act
+    def forward(self, input):
 
-def avg_pool(input, pool_size=2, strides=2):
-    """1d average pooling operation.
+        in_channels = int(input.shape[1])
+        # Since there is no padding='SAME' such feature, refer to https://discuss.pytorch.org/t/convolution-1d-and-simple-function/11606/5
+        # which says that by setting padding=(kernel_size // 2) can nail same objective provided kernel size is odd
+        if self.conv is not None:
+            conv_weight = self.conv.weight
+            conv_bias = self.conv.bias
+            self.conv = nn.Conv1d(in_channels, self.channels, kernel_size=self.kernel_size, stride=1, padding=(self.kernel_size // 2)).cuda()
+            self.conv.weight = conv_weight
+            self.conv.bias = conv_bias
+        else:
+            self.conv = nn.Conv1d(in_channels, self.channels, kernel_size=self.kernel_size, stride=1, padding=(self.kernel_size // 2)).cuda()
 
-    Arguments:
-        input (torch.Tensor): Input tensor to do convoluation.
-                                            Its shape need to be `(batch_size, channels, length)`.
-        pool_size (int): Length of a pooling. Default is `2`.
-        strides (int): Distance between two pooling steps. Default is `2`.
+        out = self.conv(input)
+        out = self.act(out)
+        return out
 
-    Returns:
-        Tensor
-    """
-    return nn.AvgPool1d(pool_size, stride=strides)(input)
 
-def deconv1d(input, channels, out_length, kernel_size, stride=1, act=nn.ReLU()):
-    """1d deconvoluation operation.
+class avg_pool(nn.Module):
 
-    Arguments:
-        input (torch.Tensor): Input tensor to do convoluation.
-                                            Its shape need to be `(batch_size, channels, length)`.
-        channels (int): Number of channels.
-        out_length (int): The output length
-        kernel_size (int): Length of 1d kernel.
-        act (activation function): Activation function after convoluation. Default is `nn.ReLU`.
+    def __init__(self, pool_size=2, strides=2):
+        super(avg_pool, self).__init__()
+        """1d average pooling operation.
 
-    Returns:
-        Tensor
-    """
+        Arguments:
+            input (torch.Tensor): Input tensor to do convoluation.
+                                                Its shape need to be `(batch_size, channels, length)`.
+            pool_size (int): Length of a pooling. Default is `2`.
+            strides (int): Distance between two pooling steps. Default is `2`.
 
-    in_length = int(input.shape[2])
-    in_channels = int(input.shape[1])
+        Returns:
+            Tensor
+        """
+        self.pool_size = pool_size
+        self.strides = strides
+        self.pool = nn.AvgPool1d(pool_size, stride=strides)
 
-    # Refer to https://pytorch.org/docs/stable/nn.html#convtranspose1d
-    # Lo = (Lin - 1) * stride - 2 * padding + kernel_size + output_padding
-    assert (in_length - 1) * stride + kernel_size > out_length
-    output_padding = (in_length - 1) * stride + kernel_size - out_length & 1
-    padding = ((in_length - 1) * stride + kernel_size - out_length + output_padding) / 2
-    output_padding = int(output_padding)
-    padding = int(padding)
+    def forward(self, input):
 
-    out = nn.ConvTranspose1d(in_channels, channels, kernel_size, stride, padding, output_padding).cuda()(input)
-    out = act(out)
+        out = self.pool(input)
+        return out
 
-    return out
+
+class deconv1d(nn.Module):
+
+    def __init__(self, channels, out_length, kernel_size, stride=1, act=nn.ReLU()):
+        super(deconv1d, self).__init__()
+        """1d deconvoluation operation.
+
+        Arguments:
+            input (torch.Tensor): Input tensor to do convoluation.
+                                                Its shape need to be `(batch_size, channels, length)`.
+            channels (int): Number of channels.
+            out_length (int): The output length
+            kernel_size (int): Length of 1d kernel.
+            act (activation function): Activation function after convoluation. Default is `nn.ReLU`.
+
+        Returns:
+            Tensor
+        """
+        self.channels = channels
+        self.out_length = out_length
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.deconv = None
+        self.act = act
+
+    def forward(self, input):
+        in_length = int(input.shape[2])
+        in_channels = int(input.shape[1])
+
+        # Refer to https://pytorch.org/docs/stable/nn.html#convtranspose1d
+        # Lo = (Lin - 1) * stride - 2 * padding + kernel_size + output_padding
+        assert (in_length - 1) * self.stride + self.kernel_size > self.out_length
+        output_padding = (in_length - 1) * self.stride + self.kernel_size - self.out_length & 1
+        padding = ((in_length - 1) * self.stride + self.kernel_size - self.out_length + output_padding) / 2
+        output_padding = int(output_padding)
+        padding = int(padding)
+
+        if self.deconv is not None:
+            deconv_weight = self.deconv.weight
+            deconv_bias = self.deconv.bias
+            self.deconv = nn.ConvTranspose1d(in_channels, self.channels, self.kernel_size, self.stride, padding, output_padding).cuda()
+            self.deconv.weight = deconv_weight
+            self.deconv.bias = deconv_bias
+        else:
+            self.deconv = nn.ConvTranspose1d(in_channels, self.channels, self.kernel_size, self.stride, padding, output_padding).cuda()
+
+        out = self.deconv(input)
+        out = self.act(out)
+
+        return out
+
 
 def conv1x1_softmax(input):
     """1d 1x1 convoluation operation. activation function is `nn.Softmax`
@@ -94,6 +143,8 @@ def conv1x1_softmax(input):
 def Signalpeptides_MCC(predictions, targets):
     targets = targets_modify(targets)
     targets = targets.detach().cpu().numpy()
+    print(targets)
+    print(predictions)
     return matthews_corrcoef(targets, predictions)
     
 def Signalpeptides_F1(predictions, targets):
@@ -105,7 +156,7 @@ def targets_modify(targets):
     """
     Since 2 represents containing signal peptides, which is positive sample
     """
-    tmp = torch.zeros(targets.shape).cuda()
+    tmp = torch.zeros(targets.shape)
     for i in range(0, targets.shape[0]):
         if targets[i] == 2:
             tmp[i] = 1
