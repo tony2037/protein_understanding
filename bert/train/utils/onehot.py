7a7e57a170fa7f5dee056a76099c224e91ba4184
ztex <ztex030640417@gmail.com>
2019-09-16 12:20:33 +0800

    Fix Pull everything onto GPU

--- bert/train/utils/onehot.py
+++ bert/train/utils/onehot.py
@@ -1,6 +1,6 @@
 import torch
 
-def index2onehot(dim, indexed_sequences):
+def index2onehot(dim, indexed_sequences, device=None):
     """
     Transform indexed sequences to one-hot encodding vectores
     @ dim: The number of class, at most case is the length of dictionary or the number of categories of tokens
@@ -8,6 +8,6 @@ def index2onehot(dim, indexed_sequences):
 
     Return: (batch_size, length, dim)
     """
-    onehot = torch.zeros(indexed_sequences.shape[0], indexed_sequences.shape[1], dim)
+    onehot = torch.zeros((indexed_sequences.shape[0], indexed_sequences.shape[1], dim), device=device)
     indexed_sequences = indexed_sequences.unsqueeze(2)
     return onehot.scatter_(2, indexed_sequences.to(torch.long), 1)
