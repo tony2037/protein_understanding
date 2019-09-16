278811c8f7819b976e4c3d228ff2ef4d7d7bff92
ztex <ztex030640417@gmail.com>
2019-09-16 12:17:26 +0800

    Feat Add one-hot size

--- bert/train/concate/concate.py
+++ bert/train/concate/concate.py
@@ -6,6 +6,7 @@ class concate(nn.Module):
     def __init__(self, output_size=32, hidden_size=128, onehot_size=29):
         super(concate, self).__init__()
         self.output_size = output_size
+        self.onehot_size = onehot_size
         self.Weights_LM = nn.Linear(hidden_size, output_size, bias=False)
         self.Weights_x = nn.Linear(onehot_size, output_size, bias=False)
         self.Bias = nn.Parameter(torch.randn(1, output_size))
