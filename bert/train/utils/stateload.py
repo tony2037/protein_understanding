3ee495d60ab1dc7f3e57a00a4ae82126f2528ad0
ztex <ztex030640417@gmail.com>
2019-11-07 13:09:26 +0800

    Feat A new function to remove layer u want to remove

--- bert/train/utils/stateload.py
+++ bert/train/utils/stateload.py
@@ -1,16 +1,26 @@
+from collections import OrderedDict
+import torch
+
 def stateLoading(model, pretrained_path):
     # Since issue: KeyError: 'unexpected key ...'
     # See https://discuss.pytorch.org/t/solved-keyerror-unexpected-key-module-encoder-embedding-weight-in-state-dict/1686/3
     # Build a new dict that contains no prefix 'module.', the length of the prefix is 7
     # original saved file with DataParallel
-    from collections import OrderedDict
-    import torch
+
     state_dict = torch.load(pretrained_path, map_location='cpu')['state_dict']
     # create new OrderedDict that does not contain `module.`
     new_state_dict = OrderedDict()
     for k, v in state_dict.items():
-        name = k[6:] # remove `module.`
+        name = k.replace('model.', '') # remove `module.`
         new_state_dict[name] = v
     # load params
     model.load_state_dict(new_state_dict)
     return model
+
+def remove_state(state_dict, to_removes):
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        if k not in to_removes:
+            name = k.replace('model.', '')
+            new_state_dict[name] = v
+    return new_state_dict
